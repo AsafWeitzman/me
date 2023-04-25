3ebf423134d28b4c4b6e6f4e9a33e02988cc821e
AsafWeitzman <asafweit1@gmail.com>
2023-04-26 01:47:04 +0300

    added some animation and starting the contact component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import { ReactComponent as ReactLogo } from "../assets/skillsIcons/react.svg";
 import { ReactComponent as CSS } from "../assets/skillsIcons/css.svg";
 import { ReactComponent as JS } from "../assets/skillsIcons/logo-javascript.svg";
@@ -10,45 +12,61 @@ import { ReactComponent as ViteJs } from "../../public/vite.svg";
 import { ReactComponent as ExpresJs } from "../assets/skillsIcons/express.svg";
 
 const logoSize = { height: 50, width: 50 };
+const logos = [
+  <JS style={logoSize} />,
+  <ExpresJs style={logoSize} />,
+  <ReactLogo style={logoSize} />,
+  <CSS style={logoSize} />,
+  <HTML style={logoSize} />,
+  <NodeJs style={logoSize} />,
+  <Github style={logoSize} />,
+  <MySql style={logoSize} />,
+  <Python style={logoSize} />,
+  <ViteJs style={logoSize} />,
+];
 
 interface IconProps {
   Icon: JSX.Element;
+  index: number;
 }
 
-const Skill = ({ Icon }: IconProps) => {
+const itemVariants = {
+  initial: { x: 0, opacity: 0 },
+  animate: { x: 0, opacity: 1 },
+};
+
+const Skill = ({ Icon, index }: IconProps) => {
   return (
-    <div
+    <motion.div
+      className="skillBox"
+      variants={itemVariants}
+      initial="initial"
+      animate="animate"
+      transition={{ duration: 0.2, delay: index * 0.2 }}
       style={{
         display: "flex",
         alignContent: "center",
         justifyContent: "center",
         borderRadius: "8px",
-        padding: "8px",
+        padding: "16px 8px",
         margin: "8px 0px",
         backgroundColor: "rgb(240, 245, 255)",
         boxShadow: "1px 1px 16px 4px rgba(109, 109, 109, 0.2)",
       }}
     >
-      {!!Icon && Icon}
-    </div>
+      {Icon}
+    </motion.div>
   );
 };
 
 const Skills = () => {
   return (
     <>
-      <h3 style={{ marginLeft: "40px", marginBottom: 0 }}>Skills</h3>
+      <h3 className="skills-title-h3">Skills</h3>
       <div className="grid-container-skills">
-        <Skill Icon={<JS style={logoSize} />} />
-        <Skill Icon={<ExpresJs style={logoSize} />} />
-        <Skill Icon={<ReactLogo style={logoSize} />} />
-        <Skill Icon={<CSS style={logoSize} />} />
-        <Skill Icon={<HTML style={logoSize} />} />
-        <Skill Icon={<NodeJs style={logoSize} />} />
-        <Skill Icon={<Github style={logoSize} />} />
-        <Skill Icon={<MySql style={logoSize} />} />
-        <Skill Icon={<Python style={logoSize} />} />
-        <Skill Icon={<ViteJs style={logoSize} />} />
+        {logos.map((logo, index) => {
+          return <Skill key={index} Icon={logo} index={index} />;
+        })}
       </div>
     </>
   );
